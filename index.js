628f19fff6a5aa69b9241794d6d77fb8b31509aa
evans <1280898239@qq.com>
2019-04-04 17:28:26 +0800

    init

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const babelCore= require('@babel/core');
 const {transRequireToDefine} = require('babel-plugin-nej-pack');
 
-const nejBundleRequireToDefinePlugin = function(){
+const nejBundleRequireToDefinePlugin = function(ops){
+    let plugins = (ops && ops.removeStrict) ? [transRequireToDefine,"remove-use-strict"] : [transRequireToDefine]
     return {
         name: 'transNejRequireToDefine',
         renderChunk(code) { 
-            return babelCore.transform(code,{"plugins":[transRequireToDefine,"remove-use-strict"]});
+            return babelCore.transform(code,{plugins});
         }
     }; 
 }
